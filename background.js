32506eb5e9b29994f70e513954aaeccd5fed91aa
o1hy <o1hy@o1hy.o1hy>
2019-06-13 16:45:47 +0800

    加了注释

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//用于传递的临时变量
 let html_tmp = {};
 
 var callback_beforeRequest = function (details) {
@@ -14,24 +15,20 @@ var callback_beforeRequest = function (details) {
 const filter = {urls: ["<all_urls>"]};
 // 额外的信息规范，可选的
 /* 监听response headers接收事件*/
-const extraInfoSpec = ["blocking", "requestBody"]; //表示阻止
+const extraInfoSpec = ["blocking", "requestBody"]; //blocking表示阻止
 //请求发送之前触发（请求的第1个事件，请求尚未创建，此时可以取消或者重定向请求）。
 var callback_onBeforeSendHeaders = function (details) {
-
-
-    // details.requestHeaders.push({name:"o1hy",value:"1"}) //可以修改头部
     return {requestHeaders: details.requestHeaders}  // 修改请求头后，进行返回内容
-
 };
 const extraInfoSpec2 = ["blocking", "requestHeaders", "extraHeaders"];
 var callback_onsendheaders = function (details) {
-
-    // console.log(details);
+    //将发出的request存储到html_tmp中
     html_tmp['id'] = details['requestId'];
     html_tmp['headers'] = details.requestHeaders;
     html_tmp['url'] = details.url;
     html_tmp['method'] = details.method;
     html_tmp['type'] = details.type;
+    //向my_vue.js发送消息
     chrome.runtime.sendMessage({greeting: html_tmp}, function (response) {});
 };
 //onSendHeaders，请求头发送之前触发（请求的第3个事件，此时只能查看请求信息，可以确认onBeforeSendHeaders事件中都修改了哪些请求头）。
@@ -41,38 +38,33 @@ var FLAG_LISTEN = false;
 
 //用于重新构造发包的请求头
 function rewriteHeaders(details) {
-    // console.log('rewrite');
-    // console.log(details);
-    // console.log(url);
-
     //只判断域名，忽略https与http。防止来回跳转。
     //if判断 当前访问的与要修改的url是否相同，防止修改了其他的url。
     if (details.url.split('://')[1] === url.split('://')[1]) {
+        //清空原来头部信息
         details.requestHeaders = [];
+        //添加用户的头部信息
         headers_all.forEach(function (header) {
             if(header.name!==""){
                 details.requestHeaders.push({'name': header.name, 'value': header.value})
             }
         });
+        //为post表单添加Content-Type
         if(details.method==="POST"){
             details.requestHeaders.push({'name':'Content-Type','value':'application/x-www-form-urlencoded'})
         }
-        // details = [];
         return {requestHeaders: details.requestHeaders};
     }
 }
 
 chrome.runtime.onMessage.addListener(function (message, sender, callback) {
-    // console.log(message);
-    //收到myvue.js的post()函数
-    // console.log(sender.url);
+
     if (sender.url !== chrome.runtime.getURL("/html/pt_dev.html")) {
-        // chrome.webRequest.onBeforeSendHeaders.removeListener(rewriteHeaders);
         return;
     }
 
     if (message.send_message) {
-
+        //下面的变量是用户要修改的内容
         url = message.send_message.url;
         headers_all = message.send_message.header_all;
         bodys_all = message.send_message.bodys_all;
@@ -82,14 +74,15 @@ chrome.runtime.onMessage.addListener(function (message, sender, callback) {
         if (methods_p === 'GET') {
             chrome.tabs.update(tabid, {url: url});
         } else {
-            // console.log(bodys_all);
+
             const post_data = JSON.stringify(bodys_all);
-            // console.log(post_data);
-            // console.log(encodeURIComponent(post_data));
+            //传递赋值
             chrome.tabs.executeScript(tabid, {code: 'let post_data = "' + encodeURIComponent(post_data) + '"; let url = "' + encodeURIComponent(url) + '"'}, function () {
+                //调用post_data.js注入表单
                 chrome.tabs.executeScript(tabid, {file: '/js/post_data.js'});
             });
         }
+        //统一监听，进行头部修改
         chrome.webRequest.onBeforeSendHeaders.addListener(
             rewriteHeaders,
             {urls: ["<all_urls>"], tabId: tabid},
@@ -100,7 +93,6 @@ chrome.runtime.onMessage.addListener(function (message, sender, callback) {
 
     // 设置监听
     FLAG_LISTEN = message.http_listen_flag;
-    // console.log('FLAG:' + FLAG_LISTEN);
 
     if (FLAG_LISTEN) {
         chrome.webRequest.onBeforeRequest.addListener(callback_beforeRequest, filter, extraInfoSpec);
--- js/myvue.js
+++ js/myvue.js
@@ -1,6 +1,7 @@
 function post(flag) {
     //用于发包的方法
     console.log('post');
+    //获取当前要做的内容
     let url = app.url;
     let headers = app.headers;
     let headers_new = app.headers_new;
@@ -9,7 +10,9 @@ function post(flag) {
     let header_all = headers.concat(headers_new);
     let bodys_all = bodys.concat(bodys_new);
     let methods_p = app.http_method;
+    //获取打开devtool的窗口id
     tabid = chrome.devtools.inspectedWindow.tabId;
+    //向background发包
     chrome.runtime.sendMessage({
         send_message: {
             url: url,
@@ -30,7 +33,7 @@ app = new Vue({
     data: {
         columns1: [
             {
-                title: 'Methods',
+                title: 'Methods', //请求方法
                 key: 'methods',
                 width: 200,
                 filters: [
@@ -49,7 +52,7 @@ app = new Vue({
 
             },
             {
-                title: 'Url',
+                title: 'Url', //请求URL
                 key: 'url'
             },
             {
@@ -87,8 +90,6 @@ app = new Vue({
                     }
                 ],
                 filterMethod(value, row) {
-                    console.log(value);
-                    console.log(row);
                     return value.indexOf(row.type) > -1;
                 }
             },
@@ -109,24 +110,10 @@ app = new Vue({
                             },
                             on: {
                                 click: () => {
-                                    app.show(params.index)
+                                    app.show(params.index) //展示当前详细信息
                                 }
                             }
                         }, 'View'),
-                        // h('Button', {
-                        //     props: {
-                        //         type: 'primary',
-                        //         size: 'small'
-                        //     },
-                        //     style: {
-                        //         marginRight: '5px'
-                        //     },
-                        //     on: {
-                        //         click: () => {
-                        //             app.showResponse(params.index)
-                        //         }
-                        //     }
-                        // }, 'Look'),
                         h('Button', {
                             props: {
                                 type: 'error',
@@ -134,7 +121,7 @@ app = new Vue({
                             },
                             on: {
                                 click: () => {
-                                    app.remove(params.index)
+                                    app.remove(params.index) //删除此行
                                 }
                             }
                         }, 'Delete')
@@ -144,6 +131,7 @@ app = new Vue({
         ],
         data1: [
             {
+                //示例数据
                 methods: 'GET',
                 url: 'https://blog.o1hy.com',
                 headers: '',
@@ -151,7 +139,7 @@ app = new Vue({
                 type: 'main_frame'
             }
         ],
-        modal1: false,
+        modal1: false, //模态框
         value: '',
         url: 'blog.o1hy.com',
         headers: [],
@@ -174,8 +162,8 @@ app = new Vue({
     },
     methods: {
         show: function (index) {
+            //展示当前行详细内容
             headers = this.data1[index].headers;
-            // console.log(app.headers);
             app.modal1 = true;
             if (headers && headers !== "") {
                 app.headers = headers;
@@ -184,7 +172,6 @@ app = new Vue({
             }
             app.http_method = this.data1[index].methods;
             app.url = this.data1[index].url;
-            console.log(this.data1[index].parameters);
             if (this.data1[index].parameters && this.data1[index].parameters !== "") {
                 app.bodys = this.data1[index].parameters;
             } else {
@@ -192,9 +179,11 @@ app = new Vue({
             }
         },
         add1: function () {
+            //添加新的头部
             app.headers.push({name: '', value: ''})
         },
         addbody: function () {
+            //添加新表单数据
             app.bodys.push({name: '', value: ''})
         },
         cancel: function () {
@@ -205,7 +194,7 @@ app = new Vue({
             post();
         },
         change: function (status) {
-            // alert(status);
+            //监控tab
             if (status) {
                 app.switch2 = false;
                 stopListenCurrentTab();
@@ -226,20 +215,17 @@ app = new Vue({
         },
         remove(index) {
             this.data1.splice(index, 1);
-        },
-        showResponse: function (index) {
-            alert(index)
         }
     }
 });
-tmptmp = [];
+
 
 function currentTabListentCallback(request) {
-    tmptmp.push(request);
+    //current tab监听模块
     let tmp = request.request;
     let tmp_body = [];
-    console.log(tmp.postData);
     if (tmp.postData) {
+        //获取post的body
         let tmp_body_1 = tmp.postData.text;
         let temp_body_list = tmp_body_1.split('&');
         temp_body_list.forEach(function (t) {
@@ -249,32 +235,41 @@ function currentTabListentCallback(request) {
     } else {
         tmp_body = ''
     }
-    console.log(tmp_body);
-    app.data1.push({
-        methods: tmp.method,
-        type: request._resourceType,
-        url: tmp.url,
-        headers: tmp.headers,
-        parameters: tmp_body
-    })
+    // console.log(tmp_body);
+    // 将内容添加到表格种
+    console.log(tmp.url);
+    if(!tmp.url.startsWith('data:image')){
+        app.data1.push({
+            methods: tmp.method,
+            type: request._resourceType,
+            url: tmp.url,
+            headers: tmp.headers,
+            parameters: tmp_body
+        })
+    }
+
 }
 
 function stopListenCurrentTab() {
+    //停止current tab的监听
     chrome.devtools.network.onRequestFinished.removeListener(currentTabListentCallback)
 }
 
 function startListenCurrentTab() {
+    //开始current tab的监听
     chrome.devtools.network.onRequestFinished.addListener(currentTabListentCallback)
 }
 
+//用来防止添加重复的request，针对all tab监听
 reuqest_ids = [];
 
 chrome.runtime.onMessage.addListener(function (message, sender, callback) {
-    // console.log(message);
+    //监听background的消息
     let tmp = message.greeting;
     let tmp_body = [];
     let form_data_array;
     try {
+        //获取post请求的body内容
         if (tmp.requestBody) {
             if (tmp.requestBody.formData) {
                 form_data_array = tmp.requestBody.formData;
@@ -283,19 +278,22 @@ chrome.runtime.onMessage.addListener(function (message, sender, callback) {
                 }
             }
         }
+        //如果没有出现过该request
+        if (reuqest_ids.indexOf(tmp.id) === -1) {
+            reuqest_ids.push(tmp.id);
+            //向表格添加信息
+            app.data1.push(
+                {
+                    methods: tmp.method,
+                    type: tmp.type,
+                    url: tmp.url,
+                    headers: tmp.headers,
+                    parameters: tmp_body
+                }
+            );
+        }
     } catch (e) {
         console.log(e)
     }
-    if (reuqest_ids.indexOf(tmp.id) === -1) {
-        reuqest_ids.push(tmp.id);
-        app.data1.push(
-            {
-                methods: tmp.method,
-                type: tmp.type,
-                url: tmp.url,
-                headers: tmp.headers,
-                parameters: tmp_body
-            }
-        );
-    }
+
 });
